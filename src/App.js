034a2a0039932bbf104a03feb628ac372107c551
Italo Rios <italo.rios@outlook.de>
2022-06-20 09:27:11 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React, { useState } from 'react';
-import Medidas from './components/Medidas/Medidas';
-import Resultado from './components/Resultado/Resultado';
+import { useState } from "react";
+import Medidas from "./components/Medidas/Medidas";
+import Resultado from "./components/Resultado/Resultado";
+import Card from "./components/UI/Card";
 
-const MEDIDAS_TOTAL = "";
+function App(props) {
+  const [parede01, setParede01] = useState("");
+  const [parede02, setParede02] = useState("");
+  const [parede03, setParede03] = useState("");
+  const [parede04, setParede04] = useState("");
 
-const App = () => {
-  const [medidas, setMedidas] = useState(MEDIDAS_TOTAL);
-
-  const addMedidasHandler = (medida) => {
-    setMedidas(medida);
-    console.log(medidas);
+  const appTestHandler = () => {
+    console.log(parede01);
   };
 
   return (
-    <div className="App">
-      <Medidas onAddMedidas={addMedidasHandler}/>
-      <Resultado resultItem={medidas}/>
-      <button onClick={addMedidasHandler}>Teste App.js</button>
-    </div>
+    <Card className="App">
+      <Medidas
+        areaParede01={(parede) => setParede01(parede)}
+        areaParede02={(parede) => setParede02(parede)}
+        areaParede03={(parede) => setParede03(parede)}
+        areaParede04={(parede) => setParede04(parede)}
+      />
+      <Resultado
+        areaTotalP01={parede01}
+        areaTotalP02={parede02}
+        areaTotalP03={parede03}
+        areaTotalP04={parede04}
+      />
+      <button onClick={appTestHandler}>Teste App</button>
+    </Card>
   );
 }
 
