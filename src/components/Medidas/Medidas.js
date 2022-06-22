03dcc5e326ee204326d83da9565879bd1ca2a2e2
Italo Rios <italo.rios@outlook.de>
2022-06-22 10:51:02 -0300

    Update Medidas.js

--- src/components/Medidas/Medidas.js
+++ src/components/Medidas/Medidas.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import MedidasItem from "./MedidasItem";
 import styles from "./Medidas.module.css";
-import Card from "../UI/Card";
 
 const Medidas = (props) => {
   const [parede01, setParede01] = useState("");
@@ -13,6 +12,95 @@ const Medidas = (props) => {
   const [esquadrias03, setEsquadrias03] = useState("");
   const [esquadrias04, setEsquadrias04] = useState("");
 
+  const buckets = (amount) => {
+    if (amount >= 90) {
+      return calc18(amount);
+    } else if (amount >= 18 && amount < 90) {
+      return calc36(amount);
+    } else if (amount >= 12.5 && amount < 18) {
+      return calc25(amount);
+    } else {
+      const total05 = calc05(amount);
+
+      return {
+        total18: 0,
+        total36: 0,
+        total25: 0,
+        total05: total05,
+      };
+    }
+  };
+
+  const amountRest = (bucket, amount) => {
+    const amountBucket = Math.floor(amount / bucket);
+    let rest = (amount % bucket).toFixed(2);
+    return { amount: amountBucket, rest: rest };
+  };
+
+  const calc05 = (amount) => {
+    const temp = amountRest(2.5, amount);
+    let total = 0;
+    if (temp.rest > 0) {
+      total = temp.amount + 1;
+    } else {
+      total = temp.amount;
+    }
+
+    return total;
+  };
+
+  const calc25 = (amount) => {
+    let temp = amountRest(12.5, amount);
+    const total25 = temp.amount;
+    let total05 = 0;
+    if (temp.rest > 0) {
+      total05 = calc05(temp.rest);
+    }
+
+    return { total18: 0, total36: 0, total25: total25, total05: total05 };
+  };
+
+  const calc36 = (amount) => {
+    let temp = amountRest(18.0, amount);
+    const total36 = temp.amount;
+    let total25 = 0;
+    let total05 = 0;
+    if (temp.rest >= 12.5 && temp.rest < 18.0) {
+      temp = calc25(temp.rest);
+      total25 = temp.total25;
+      total05 = temp.total05;
+    } else {
+      total05 = calc05(temp.rest);
+    }
+
+    return { total18: 0, total36: total36, total25: total25, total05: total05 };
+  };
+
+  const calc18 = (amount) => {
+    let temp = amountRest(90, amount);
+    const total18 = temp.amount;
+    let total36 = 0;
+    let total25 = 0;
+    let total05 = 0;
+    if (temp.rest >= 18.0 && temp.rest < 90) {
+      temp = calc36(temp.rest);
+      total36 = temp.total36;
+      total25 = temp.total25;
+      total05 = temp.total05;
+    } else if (temp.rest >= 12.5 && temp.rest < 18.0) {
+      temp = calc25(temp.rest);
+      total25 = temp.total25;
+      total05 = temp.total05;
+    } else {
+      total05 = calc05(temp.rest);
+    }
+    return {
+      total18: total18,
+      total36: total36,
+      total25: total25,
+      total05: total05,
+    };
+  };
 
   const calcAreaTintaHandler = () => {
     const areaTotal01 = parede01 - esquadrias01;
@@ -24,10 +112,35 @@ const Medidas = (props) => {
     props.areaParede03(areaTotal03);
     props.areaParede04(areaTotal04);
 
-  }
+    let total =
+      parseFloat(areaTotal01) +
+      parseFloat(areaTotal02) +
+      parseFloat(areaTotal03) +
+      parseFloat(areaTotal04);
+
+    total = buckets(total);
+    props.quanTinTemp01(total);
+
+    // let amount1 = parseFloat(areaTotal01);
+    // amount1 = buckets(amount1);
+    // props.quanTinTemp01(amount1);
+
+    // let amount2 = parseFloat(areaTotal02);
+    // amount2 = buckets(amount2);
+    // props.quanTinTemp02(amount2);
+
+    // let amount3 = parseFloat(areaTotal03);
+    // amount3 = buckets(amount3);
+    // props.quanTinTemp03(amount3);
+
+    // let amount4 = parseFloat(areaTotal04);
+    // amount4 = buckets(amount4);
+    // props.quanTinTemp04(amount4);
+  };
 
   return (
-    <Card>
+    <div>
+    <div className="medidas">
       <div className={styles.medidas}>
         <label>Digite quantas Portas e Janelas tem cada Parede:</label>
         <div className={styles["medidas-items"]}>
@@ -52,9 +165,16 @@ const Medidas = (props) => {
             onGetAreaEsquadrias={(esquadria) => setEsquadrias04(esquadria)}
           />
         </div>
-        <button onClick={calcAreaTintaHandler} form='medidas-form' type="submit">Exibir Resultados</button>
+        <button
+          onClick={calcAreaTintaHandler}
+          form="medidas-form"
+          type="submit"
+        >
+          Exibir Resultados
+        </button>
       </div>
-    </Card>
+    </div>
+    </div>
   );
 };
 
