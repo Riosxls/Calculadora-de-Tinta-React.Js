4ec230c0e9798b8336e7a1352e7328f00b5e013e
Italo Rios <italo.rios@outlook.de>
2022-06-22 10:53:57 -0300

    Update Resultado.js

--- src/components/Resultado/Resultado.js
+++ src/components/Resultado/Resultado.js
@@ -1,66 +1,83 @@
 import React from "react";
-import Card from "../UI/Card";
 import styles from "./Resultado.module.css";
 
 const Resultado = (props) => {
-
   return (
-    <Card>
-      <div className={styles.resultado}>
+    <div className={styles.resultado}>
+      <div className={styles["resultado-parede"]}>
         <div className={styles["resultado-item"]}>
           <label className={styles["resultado-title"]}>1ª Parede</label>
           <div className={styles.textarea}>
             <label>
-              Área <br /> Total:
+              Área da <br /> Parede: {props.areaTotalP01}
             </label>
-            <p>{props.areaTotalP01}</p>
             <label>
-              Quantidade <br /> de Tinta:
+              Litros <br /> de Tinta: {props.qtdTinta01.total05}
             </label>
-            <p></p>
           </div>
         </div>
         <div className={styles["resultado-item"]}>
           <label className={styles["resultado-title"]}>2ª Parede</label>
           <div className={styles.textarea}>
             <label>
-              Área <br /> Total: {props.parede02}
+              Área da <br /> Parede: {props.areaTotalP02}
             </label>
-            <p>{props.areaTotalP02}</p>
             <label>
-              Quantidade <br /> de Tinta:
+              Litros <br /> de Tinta: {props.qtdTinta02.total05}
             </label>
-            <p></p>
           </div>
         </div>
         <div className={styles["resultado-item"]}>
           <label className={styles["resultado-title"]}>3ª Parede</label>
           <div className={styles.textarea}>
             <label>
-              Área <br /> Total:
+              Área da <br /> Parede:
             </label>
             <p>{props.areaTotalP03}</p>
             <label>
-              Quantidade <br /> de Tinta:
+              Litros <br /> de Tinta:
             </label>
-            <p></p>
+            {/* <p>Amount buckets of 18L: {props.quanTinTotal03.total18}</p>
+            <p>Amount buckets of 3.6L: {props.quanTinTotal03.total36}</p>
+            <p>Amount buckets of 2.5L: {props.quanTinTotal03.total25}</p>
+            <p>Amount buckets of 0.5L: {props.quanTinTotal03.total05}</p> */}
           </div>
         </div>
         <div className={styles["resultado-item"]}>
           <label className={styles["resultado-title"]}>4ª Parede</label>
           <div className={styles.textarea}>
             <label>
-              Área <br /> Total:
+              Área da <br /> Parede:
             </label>
             <p>{props.areaTotalP04}</p>
             <label>
-              Quantidade <br /> de Tinta:
+              Litros <br /> de Tinta:
             </label>
-            <p></p>
+            {/* <p>Amount buckets of 18L: {props.quanTinTotal04.total18}</p>
+            <p>Amount buckets of 3.6L: {props.quanTinTotal04.total36}</p>
+            <p>Amount buckets of 2.5L: {props.quanTinTotal04.total25}</p>
+            <p>Amount buckets of 0.5L: {props.quanTinTotal04.total05}</p> */}
           </div>
         </div>
       </div>
-    </Card>
+      <div>
+        <label>Você vai precisar de:</label>
+      </div>
+      <div className={styles["resultado-tinta"]}>
+        <p className={styles["resultado-item"]}>
+          {props.qtdTinta01.total18} Latas de 18L
+        </p>
+        <p className={styles["resultado-item"]}>
+          {props.qtdTinta01.total36} Latas de 3.6L
+        </p>
+        <p className={styles["resultado-item"]}>
+          {props.qtdTinta01.total25} Latas de 2.5L
+        </p>
+        <p className={styles["resultado-item"]}>
+          {props.qtdTinta01.total05} Latas de 0.5
+        </p>
+      </div>
+    </div>
   );
 };
 
